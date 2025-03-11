b4c1d2beb8b57aa29eb9e41f1f07d71170d65a5d
sunpressed <isgamehard@gmail.com>
2025-03-11 10:53:36 +0300

    ss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.main")
+@section('title', 'Главная страница')
+
+
+
+@section('body')
+<h1>
+    Lorem ipsum dolor sit amet.
+</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, dolores ducimus minima praesentium ut voluptatem.
+</p>
+@endsection
+
